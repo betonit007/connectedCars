2ede7c4e570f398d46e93180048c569bbb5ada1b
betonit007 <timnagorski@hotmail.com>
2019-09-09 17:26:46 -0400

    final changes before implementing tailwindcss

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import React, { Fragment } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 import Navbar from './components/layout/Navbar';
-import Home from './components/layout/pages/Home';
-
 
 const App = () => {
   return (
@@ -11,10 +8,10 @@ const App = () => {
      <Router >
             <Fragment>
               <Navbar />
-              <div className="container">
+              <div>
                 {/* <Alerts /> */}
                 <Switch>
-                  <Route exact path='/' component={Home} />
+                  {/* <Route exact path='/' component={Home} />
                   {/* <Route exact path='/about' component={About} />
                   <Route exact path='/register' component={Register} />
                   <Route exact path='/login' component={Login} /> */}
