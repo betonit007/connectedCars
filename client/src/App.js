65fa0d1f17d1c29411c6ed64b7ceed3dc7b88967
betonit007 <timnagorski@hotmail.com>
2019-09-10 20:07:45 -0400

    working on mobil navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,20 @@ import Navbar from './components/layout/Navbar';
 const App = () => {
   return (
     <div>
-     <Router >
-            <Fragment>
-            <div className="bg-blue-100">Hello World!</div>
-              <Navbar />
-              <div>
-                {/* <Alerts /> */}
-                <Switch>
-                  {/* <Route exact path='/' component={Home} />
+      <Router >
+        <Fragment>
+          <Navbar />
+          <div>
+            {/* <Alerts /> */}
+            <Switch>
+              {/* <Route exact path='/' component={Home} />
                   {/* <Route exact path='/about' component={About} />
                   <Route exact path='/register' component={Register} />
                   <Route exact path='/login' component={Login} /> */}
-                </Switch>
-              </div>
-            </Fragment>
-          </Router>
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
     </div>
   )
 }
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+//import AuthContext from '../../context/auth/authContext';
+//import ContactContext from '../../context/contact/contactContext';
+
+import CarIcon from './CarIcon.js';
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
-        </div>
-    )
+
+  const onLogout = () => {
+    alert('logout')
+  }
+
+  const authLinks = (
+    <div className='flex'>
+      <li className='px-5'>
+        Hello User
+            </li>
+      <li>
+        <a onClick={onLogout} href='#!'>Logout</a>
+      </li>
+    </div>
+  )
+
+  const guestLinks = (
+    <ul className='flex justify-end'>
+      <li className='px-5 text-white'>
+        <Link to="/register" >Register</Link>
+      </li>
+
+      <li>
+        <Link to="/login" className='text-white'>Login</Link>
+      </li>
+    </ul>
+  )
+
+
+  return (
+    <div className="bg-blue-500 w-full h-20 flex items-center flex-wrap">
+      <div className="flex w-full px-5 justify-center md:w-1/3">
+        <CarIcon />
+        <span className='text-2xl text-white'>Connected Cars</span>
+      </div>
+      <div className="flex w-full px-5 justify-center md:w-2/3 md:justify-end">
+        {!true ? authLinks : guestLinks}
+      </div>
+    </div >
+  )
 }
 
-export default Navbar
+export default Navbar;
