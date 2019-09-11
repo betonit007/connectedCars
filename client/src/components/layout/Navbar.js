576f1c4a126544190afac0851588b42b0a71a685
betonit007 <timnagorski@hotmail.com>
2019-09-10 21:06:14 -0400

    navbar media queries

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -37,11 +37,11 @@ const Navbar = () => {
 
   return (
     <div className="bg-blue-500 w-full h-20 flex items-center flex-wrap">
-      <div className="flex w-full px-5 justify-center md:w-1/3">
+      <div className="flex w-full px-5 justify-center border-b md:w-1/3 md:border-none md:px-5">
         <CarIcon />
-        <span className='text-2xl text-white'>Connected Cars</span>
+        <span className='text-2xl text-white italic'>Connected Cars</span>
       </div>
-      <div className="flex w-full px-5 justify-center md:w-2/3 md:justify-end">
+      <div className="flex w-full justify-center md:w-2/3 md:justify-end md:pr-10">
         {!true ? authLinks : guestLinks}
       </div>
     </div >
