916c5a559f42f3381a54da666a012d8eb3f404b0
betonit007 <timnagorski@hotmail.com>
2019-09-23 17:22:10 -0400

    cont'd working on styling for homepage

--- client/src/components/layout/Choices.js
+++ client/src/components/layout/Choices.js
@@ -1,4 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import buyPic from './img/buy.jpg';
+import couple from './img/couple.jpg';
+import research from './img/research.jpg';
+import cleanFront from './img/cleanfront.jpg';
 
 const Choices = () => {
 
@@ -16,27 +20,39 @@ const Choices = () => {
 		switch (choice) {
 			case 1:
 				return (
-					<div className="bg-red-600">
-						<button onClick={() => handleChoice(1)}>Choice 1</button><button onClick={() => handleChoice(2)}>Choice 2</button><button onClick={() => handleChoice(3)}>Choice 3</button>
+					<div className="customHeight" style={{ backgroundImage: `url("${cleanFront}")`, backgroundSize: `cover` }}>
+						<div className='w-1/2 text-white m-auto flex justify-around pt-5'>
+							<button onClick={() => handleChoice(1)}>Choice 1</button>
+							<button onClick={() => handleChoice(2)}>Choice 2</button>
+							<button onClick={() => handleChoice(3)}>Choice 3</button>
+						</div>
 					</div>
 				)
 			case 2:
 				return (
-					<div className="bg-green-600">
-						<button onClick={() => handleChoice(1)}>Choice 1</button><button onClick={() => handleChoice(2)}>Choice 2</button><button onClick={() => handleChoice(3)}>Choice 3</button>
+					<div className="customHeight" style={{ backgroundImage: `url("${couple}")`, backgroundSize: `cover` }}>
+						<div className='w-1/2 m-auto flex justify-around pt-5'>
+							<button onClick={() => handleChoice(1)}>Choice 1</button>
+							<button onClick={() => handleChoice(2)}>Choice 2</button>
+							<button onClick={() => handleChoice(3)}>Choice 3</button>
+						</div>
 					</div>
 				)
 			case 3:
 				return (
-					<div className="bg-blue-600">
-						<button onClick={() => handleChoice(1)}>Choice 1</button><button onClick={() => handleChoice(2)}>Choice 2</button><button onClick={() => handleChoice(3)}>Choice 3</button>
+					<div className="customHeight" style={{ backgroundImage: `url("${research}")`, backgroundSize: `cover` }}>
+						<div className='w-1/2 m-auto flex justify-around pt-5'>
+							<button onClick={() => handleChoice(1)}>Choice 1</button>
+							<button onClick={() => handleChoice(2)}>Choice 2</button>
+							<button onClick={() => handleChoice(3)}>Choice 3</button>
+						</div>
 					</div>
 				)
 		}
 	}
 
 	return (
-		<div className="container m-auto">
+		<div className="w-full">
 			{renderChoice()}
 		</div>
 
