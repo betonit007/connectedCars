cc1319b5aaf1e9bca8cce6f86fe0bed2898e5e14
betonit007 <timnagorski@hotmail.com>
2019-10-04 16:03:43 -0400

    trying again

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const path = require('path');
+const path = require('path'); //not used in development
 
 const app = express();
 
@@ -13,6 +13,8 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/cars', require('./routes/cars'));
 
+
+
 // Serve static assets in production
 
 if(process.env.NODE_ENV ===  'production') {
@@ -22,6 +24,8 @@ if(process.env.NODE_ENV ===  'production') {
 
 
 app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+
+//app.get('/', (req, res) => res.json({msg: 'Welcome to Connected Cars'})); //just used for development
 
 const PORT = process.env.PORT || 5000;
 
