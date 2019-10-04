f774be9b988ce5b040b5a63578f80cb5943242ee
betonit007 <timnagorski@hotmail.com>
2019-10-04 17:14:46 -0400

    changed tailwind dependencies from dev to regular

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const path = require('path'); //not used in development
+const path = require('path');
 
 const app = express();
 
@@ -13,19 +13,14 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/cars', require('./routes/cars'));
 
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
 
-
-// Serve static assets in production
-
-if(process.env.NODE_ENV ===  'production') {
-    //Set static folder
-    app.use(express.static('client/build'))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
 
-app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-
-//app.get('/', (req, res) => res.json({msg: 'Welcome to Connected Cars'})); //just used for development
 
 const PORT = process.env.PORT || 5000;
 
