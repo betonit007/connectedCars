6b2fa1af2e7bfb39dd99fd3a6b68e3cef045b738
betonit007 <timnagorski@hotmail.com>
2019-10-14 14:12:29 -0400

    changed to production settings

--- server.js
+++ server.js
@@ -13,8 +13,15 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/cars', require('./routes/cars'));
 
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
 
-app.get('/', (req, res) => res.json({msg: 'Welcome to Connected Cars'}));
+    app.get('*', (res, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
+// Dev Code Below
+//app.get('/', (req, res) => res.json({msg: 'Welcome to Connected Cars'}));
 
 const PORT = process.env.PORT || 5000;
 
